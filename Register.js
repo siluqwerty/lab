fcd67d4510f792d92bc9975584dd7948843a77c5
siluqwerty <shailendra11051989@gmail.com>
2024-08-20 20:48:00 +0530

    register page is set

--- Register.js
+++ Register.js
@@ -6,28 +6,40 @@ export default function Register() {
         <div>
             <Header />
 
-            <form>
+            <center>
                 <h2>Registration Form</h2>
-                <h3>First Name</h3><input type='text' />
-                <h3>Last Name</h3><input type='text' />
-                <h3>Mob. No.</h3><input type='number' />
-                <h3>Adhar No.</h3><input type='number' />
-                <h3>Select Course ID</h3><select>
-                    <option>01FWD:Full Stack Web Development</option>
-                    <option>02PHP:Hypertext Preprocessor</option>
-                    <option>03PY:Python</option>
-                    <option>04JS:Java Script</option>
-                </select>
-                <h2>Educational Qualification</h2>
-                <h3>High School</h3>
-                <h4>School Name</h4><input type='text' />
-                <h4>Board Name</h4><input type='text' /><h4>Select Passout Date</h4><input type='date' />
-                <h3>Graduation</h3>
-                <h4>Institute Name</h4><input type='text' />
-                <h4>Course Name</h4><input type='text' />
-                <h4>Select Passout Date</h4><input type='date' />
-                <button>REGISTER</button>
-            </form>
+                <form>
+                    <table>
+                        <tr>
+                            <td><h3>First Name</h3><input type='text' /></td>
+                            <td><h3>Last Name</h3><input type='text' /></td>
+                            <td><h3>Mob. No.</h3><input type='number' /></td>
+                            <td><h3>Adhar No.</h3><input type='number' /></td>
+                            <td><h3>Select Course ID</h3><select>
+                                <option>01FWD:Full Stack Web Development</option>
+                                <option>02PHP:Hypertext Preprocessor</option>
+                                <option>03PY:Python</option>
+                                <option>04JS:Java Script</option>
+                            </select></td>
+                        </tr>
+                        <tr>
+                            <td><h2>Educational <br></br>Qualification</h2></td>
+                            <td><h3>High School</h3></td>
+                            <td><h4>School Name</h4><input type='text' /></td>
+                            <td><h4>Board Name</h4><input type='text' /></td>
+                            <td><h4>Select Passout Date</h4><input type='date' /></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td><h3>Graduation</h3></td>
+                            <td> <h4>Institute Name</h4><input type='text' /></td>
+                            <td> <h4>Course Name</h4><input type='text' /></td>
+                            <td><h4>Select Passout Date</h4><input type='date' /></td>
+                        </tr>
+                    </table>
+                    <input type='submit' className='regBtn'></input>
+                </form>
+            </center>
         </div>
     )
 }
